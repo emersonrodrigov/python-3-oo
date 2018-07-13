3f16e1f2c11cd31a2228152cc9940de9d3bb1d92
emersonrvsilva <gft.emerson.silva@br.experian.com>
2018-07-13 14:51:18 -0300

    aula 1

--- alura_aula_1.py
+++ alura_aula_1.py
@@ -1,5 +1,5 @@
 
-import conta
+import conta_service
 
 #  Dados da conta - Orientacao Objeto
 
@@ -15,5 +15,13 @@ c1 = {"numero": 123, "titular": "Nico", "saldo": 55.0, "limite": 1000.0}
 
 print("Dados conta 2 {} ".format(c1))
 
-print("Nova conta {}".format(conta.cria_conta(123, "Nico", 55.0, 1000.0)))
+print("Nova conta {}".format(conta_service.cria_conta(123, "Nico", 55.0, 1000.0)))
 
+
+#  testando funcoes conta
+
+conta = conta_service.cria_conta(123, "Nico", 55.0, 1000.0)
+conta_service.deposita(conta, 15.0)
+conta_service.extrato(conta)
+conta_service.saque(conta, 20.0)
+conta_service.extrato(conta)
